22929d95f746b4a73d120a853237780869102c7b
stnkvld <stnkvld@gmail.com>
2017-03-27 07:13:23 +0300

    Первый коммит

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var sliderMain = new Swiper ("#slider-main", {
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+
+    $("#work-process .wrapper .left li").on("click", function() {
+        $("#work-process .wrapper .left li").removeClass("active");
+        $("#work-process .wrapper .right > div").css("opacity", 0);
+        switch ($(this).attr("class")) {
+            case "consultation":
+                $(".right .consultation").css("opacity", 1);
+                break;
+            case "design":
+                $(".right .design").css("opacity", 1);
+                break;
+            case "delivery":
+                $(".right .delivery").css("opacity", 1);
+                break;
+            case "assembly":
+                $(".right .assembly").css("opacity", 1);
+                break;
+            case "escort":
+                $(".right .escort").css("opacity", 1);
+        }
+        $(this).addClass("active");
+    });
+});
