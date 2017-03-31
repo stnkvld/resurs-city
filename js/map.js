9f4937f6bf14c4bba3e762c9541ba8489ea17d6b
stnkvld <stnkvld@gmail.com>
2017-03-31 07:12:05 +0300

    Небольшие правки

--- js/map.js
+++ js/map.js
@@ -8,6 +8,9 @@ function init(){
         controls: []
     }, {suppressMapOpenBlock: true});
 
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('drag');
+
     myPlacemark = new ymaps.Placemark([59.926372, 30.318734], {}, {
         iconLayout: 'default#image',
         iconImageHref: 'images/marker-logo.png',
